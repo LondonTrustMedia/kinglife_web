45cd8d1be3e0edad76910fb8a7c6a8c0859d40ed
Gaurav Giri <grv@grv.gr>
2018-02-23 22:19:44 +0000

    initial commit

---
+++ web/footerin.php
@@ -0,0 +1,30 @@
+
+<footer>
+    <div class="container white">
+        <div class="row text-center">
+            <div class="col-md-12 copyright">
+                <a class="page-scroll" href="#about"><img src="../img/logov.svg" class="footerimg">
+                </a>
+                <br>
+                <br>
+                              <a href="https://www.youtube.com/channel/UCZXEMj23xXnAvXhtfJ50TNw" class="white" target="_blank"><i class="fa fa-lg fa-youtube"></i></a>&nbsp; 
+                <a href="https://www.instagram.com/KinglifeFamily/" class="white" target="_blank"><i class="fa fa-lg fa-instagram" ></i></a> &nbsp;
+                <a href="https://www.facebook.com/KinglifeFamily" class="white" target="_blank"><i class="fa fa-lg fa-facebook" ></i></a> &nbsp;
+                <a href="https://www.reddit.com/user/KinglifeFamily/" class="white" target="_blank"><i class="fa fa-lg fa-reddit" ></i></a> &nbsp;
+                <a href="https://twitter.com/KinglifeFamily" class="white" target="_blank"><i class="fa fa-lg fa-twitter" ></i></a>
+                <a href="http://www.datpiff.com/profile/KinglifeFamily" class="white"  target="_blank"><i class="icon-datpiff fa-lg"></i></a>
+                <a href="https://soundcloud.com/user-199609692" class="white"  target="_blank"><i class="icon-soundcloud fa-lg"></i></a>
+                <a href="http://snapchat.com/add/KinglifeFamily" class="white"  target="_blank"><i class="icon-snapchat fa-lg"></i></a>
+
+
+
+                    <br>
+                    <br>
+                    <p>Copyright © 2017</p>
+
+            </div>
+
+
+        </div>
+    </div>
+</footer>
